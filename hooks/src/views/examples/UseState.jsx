6012b8d4dc6699fca4c83d444df708b5b818865a
davinegreiiros <davi.negreiiros36@gmail.com>
2023-09-04 20:48:23 -0300

    exercicio

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+    const [count, setCount] = useState(0)
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+                subtitle="Estado em componentes funcionais!"/>
+            <SectionTitle title="Exercicio 1"/>
+
+            <SectionTitle title="Exercicio 2"/>
         </div>
     )
 }
